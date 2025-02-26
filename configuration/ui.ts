5b43c038ee03c1cfe9c597ec97f554359869f94e
rbeard1612 <62758119+rbeard1612@users.noreply.github.com>
2025-02-26 14:07:19 -0500

    Update ui.ts
    update chat name

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Baseball Data Chat`;
+export const CHAT_HEADER: string = `Atlanta Braves Baseball Chat`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Built by Beardo 2025`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
